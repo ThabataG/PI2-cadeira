0dbf6c097f98a0bbf98659090f36463e74f08855
Cotrim <cotrim149@gmail.com>
2015-10-12 14:10:59 -0300

    Change detect event to trigger with pull down resistor and refactoring serialObject class with static methods #2

--- joystick/Joystick.py
+++ joystick/Joystick.py
@@ -1,17 +1,19 @@
-import RPi.GPIO as GPIO
 from serialObject import *
 
 pinNumber = 11
+serialObject = None
 
 class Joystick(object):
     def __init__(self):
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(pinNumber,GPIO.IN)
+        GPIO.setup(pinNumber,GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
         self.setupInterruption()
 
     def setupInterruption(self):
-        initSerialObject()
+        global serialObject
+        serialObject = SerialObject.initSerialObject()
         GPIO.add_event_detect(pinNumber, GPIO.FALLING, callback=readMsg, bouncetime=300)
+        print("Setup interruption")
 
     def translateCommandFromMSP(self,message):
         message = message.rstrip('\n')
@@ -20,3 +22,30 @@ class Joystick(object):
 
     def sendMessageToMSP(self,command):
         return False
+
+def readMsg(channel):
+    print(channel)
+    global serialObject #to set serial object
+    serialObject = SerialObject.connectWithSerialPort(serialObject)
+    if serialObject.isOpen():
+        try:
+            serialObject.flushInput() # Flush input buffer, discarding all its contents
+            serialObject.flushOutput() # Flush output buffer, aborting current output and discard all that is in buffer
+    		# Write data
+    		#s.write("AT+CSQ")
+    		#print("write data: AT+CSQ")
+    		#time.sleep(0.5)  				# Give the serial port sometime to receive the data
+            try:
+                response = serialObject.readline()
+#                for char in response:
+#                    print(char)
+                print("Received Msg")
+                print(response)
+            except KeyboardInterrupt:
+                GPIO.cleanup()
+
+            serialObject.close()
+        except Exception as e:
+            print("error communicating...: " + str(e))
+    else:
+    	print("cannot open serial port ")
--- joystick/serialObject.py
+++ joystick/serialObject.py
@@ -1,47 +1,29 @@
 import serial, time
+import RPi.GPIO as GPIO
 
-serialObject = serial.Serial()
+class SerialObject(object):
 
-def initSerialObject():
-    serialObject.port = "/dev/ttyACM0"
-    serialObject.baudrate = 9600
-    serialObject.bytesize = serial.EIGHTBITS 			# Number of bits per bytes
-    serialObject.parity = serial.PARITY_NONE 			# Set parity check: no parity
-    serialObject.stopbits = serial.STOPBITS_ONE 		# Number of stop bits
-    serialObject.timeout = 1            				# Timeout block read
-    serialObject.xonxoff = False     					# Disable software flow control
-    serialObject.rtscts = False     					# Disable hardware (RTS/CTS) flow control
-    serialObject.dsrdtr = False       					# Disable hardware (DSR/DTR) flow control
-    serialObject.writeTimeout = 2     					# Timeout for write
+    @staticmethod
+    def initSerialObject():
+        serialObject = serial.Serial()
 
-def connectWithSerialPort():
-    try:
-    	serialObject.open()
-    except Exception as e:
-    	print("error open serial port: " + str(e))
-    	exit()
+        serialObject.port = "/dev/ttyACM0"
+        serialObject.baudrate = 9600
+        serialObject.bytesize = serial.EIGHTBITS 			# Number of bits per bytes
+        serialObject.parity = serial.PARITY_NONE 			# Set parity check: no parity
+        serialObject.stopbits = serial.STOPBITS_ONE 		# Number of stop bits
+        serialObject.timeout = 1            				# Timeout block read
+        serialObject.xonxoff = False     					# Disable software flow control
+        serialObject.rtscts = False     					# Disable hardware (RTS/CTS) flow control
+        serialObject.dsrdtr = False       					# Disable hardware (DSR/DTR) flow control
+        serialObject.writeTimeout = 2     					# Timeout for write
+        return serialObject
 
-def readMsg(channel):
-    connectWithSerialPort()
-    if serialObject.isOpen():
+    @staticmethod
+    def connectWithSerialPort(serialObject):
         try:
-            serialObject.flushInput() # Flush input buffer, discarding all its contents
-            serialObject.flushOutput() # Flush output buffer, aborting current output and discard all that is in buffer
-    		# Write data
-    		#s.write("AT+CSQ")
-    		#print("write data: AT+CSQ")
-    		#time.sleep(0.5)  				# Give the serial port sometime to receive the data
-            try:
-                response = serialObject.readline()
-#                for char in response:
-#                    print(char)
-                print("Received Msg")
-                print(response)
-            except KeyboardInterrupt:
-                GPIO.cleanup()
-
-            serialObject.close()
+        	serialObject.open()
         except Exception as e:
-            print("error communicating...: " + str(e))
-    else:
-    	print("cannot open serial port ")
+        	print("error open serial port: " + str(e))
+        	exit()
+        return serialObject
