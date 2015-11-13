d0bde32da9408b6aa1e82212caf3e100f4087716
Cotrim <cotrim149@gmail.com>
2015-11-13 20:49:38 -0200

    Removed test to send message to MSP

--- test/testJoystick.py
+++ test/testJoystick.py
@@ -17,7 +17,6 @@ class testJoystick(unittest.TestCase):
     def setUp(self):
         global joy
         joy = Joystick()
-        GPIO.cleanup()
 
     def tearDown(self):
         joy = None
@@ -31,9 +30,6 @@ class testJoystick(unittest.TestCase):
         receivedMsg = joy.readMsg()
         self.assertEqual(receivedMsg,"ab\n")
 
-#using scale to engines: 1 to 10 => Foward
-#                       -10 to -1 => Backward
-#                        0 => Stop
     @patch.object(Joystick, 'readMsg')
     def testTranslateMsgFromMSPToCommandXAndY(self,mock_output):
         listOfValues = [200,201,10]
@@ -43,17 +39,6 @@ class testJoystick(unittest.TestCase):
         commandX = 200
         commandY = 201
         self.assertEqual(command,[commandX,commandY])
-
-    @patch.object(Joystick,'readMsg')
-    def testSendMessageToMSPEngines(self,mock_output):
-        #[commandX,commandY,markerCharacter]
-        listOfValues = [200,201,10]
-        mock_output.return_value=bytes(listOfValues)
-        receivedMsg = joy.readMsg()
-        command = joy.translateCommandFromMSP(receivedMsg)
-        success = joy.sendMessageToEnginesMSPs(command)
-
-        self.assertEqual(success,True)
 
     @patch.object(Joystick,'readMsg')
     def testReceiveMessageWithMissingValue(self,mock_output):
