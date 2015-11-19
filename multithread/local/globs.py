13c83e3857588009a3e5e461d0445018fa3570d7
Duerno <felipeduernoal@gmail.com>
2015-11-18 22:34:47 -0200

    Change global lock to condition thread

--- multithread/local/globs.py
+++ multithread/local/globs.py
@@ -3,8 +3,8 @@ import threading
 def init_globals():
 	# Globals
 	global lock
-	lock = threading.Lock()
+	lock = threading.Condition()
 	global coordinates
 	coordinates = {'x': 0, 'y': 1}
 	global wait
-	wait = False
+	wait = True
