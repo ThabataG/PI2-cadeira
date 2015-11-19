e0db4620287f4e5422e5a66030613c83d0081d8c
Duerno <felipeduernoal@gmail.com>
2015-11-18 22:45:55 -0200

    Optimize serial port search, allow serial write timeout, improving logging, insert conditional thread variable, send data via serial port

--- multithread/local/motorController.py
+++ multithread/local/motorController.py
@@ -20,13 +20,14 @@ class MotorController(threading.Thread):
     def run(self):
         while True:
             while globs.wait:
+                #print("Inside while wait!")
                 continue
             isOpen = False
             port = 0
             while True:
-                while port < 10:
+                while port < 2:
                     try:
-                        s = SerialObject.initSerialObject("/dev/ttyACM" + str(port), False)
+                        s = SerialObject.initSerialObject("/dev/ttyACM" + str(port), True)
                         isOpen = True
                         port += 1
                         break
@@ -37,15 +38,15 @@ class MotorController(threading.Thread):
                 if isOpen:
                     try:
                         s.flushInput()
-                        rcv_str = s.read(100)
+                        rcv_str = s.read(10)
                         if(len(rcv_str) == 0):
                             break
                         else:
-                            logging.info("I don't want you, joy!")
+                            logging.info("Motor: I don't want you, joy!")
                     except serial.SerialException:
-                        logging.info("SerialException: port closed.")
+                        logging.info("Motor: SerialException: port closed.")
                     except Exception:
-                        logging.info("Flush input buffer error. (?)")
+                        logging.info("Motor: Flush input buffer error. (?)")
                     if s.isOpen():
                         s.close()
                     isOpen = False
@@ -60,26 +61,31 @@ class MotorController(threading.Thread):
 					#s.write("AT+CSQ")
 					#print("write data: AT+CSQ")
 					#time.sleep(0.5)  				# Give the serial port sometime to receive the data
-                    logging.info("start writing...")
+                    logging.info("Motor: start writing...")
                     while True:
                         try:
                             #write information
+                            #print("Writing information")
+                            globs.lock.acquire()
                             if self.x != globs.coordinates['x'] or self.y != globs.coordinates['y']:
                                 print(str(globs.coordinates['x']) + ',' + str(globs.coordinates['y']))
                                 self.x = globs.coordinates['x']
                                 self.y = globs.coordinates['y']
-                                #success = SerialObject.writeWithSerial([self.x,self.y])
+                                s.flushOutput()
+                                success = SerialObject.writeWithSerial(s,[self.x,self.y])
                             else:
-                                logging.info("Motor: failed to write in serial.")
-                                break
+                                #print("Else writing information")
+                                globs.lock.wait()
+                                logging.info("Motor: X and Y not change.")
+                                #break
+                            globs.lock.release()
                         except KeyboardInterrupt:
-                            logging.info("Exiting via keyboard interruption...")
+                            logging.info("Motor: Exiting via keyboard interruption...")
                             s.close()
-                            logging.info("communication closed.")
+                            logging.info("Motor: communication closed.")
                             exit()
                     s.close()
                 except Exception as e:
-                    logging.info("error communicating...: " + str(e))
+                    logging.info("Motor: error communicating...: " + str(e))
             else:
-                logging.info("cannot open serial port ")
-
+                logging.info("Motor: cannot open serial port ")
