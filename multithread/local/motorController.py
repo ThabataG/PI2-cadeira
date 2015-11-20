8a4cd34835d48e6cf08416d6200400acd5f1e994
Cotrim <cotrim149@gmail.com>
2015-11-20 19:11:45 -0200

    Refactor class

--- multithread/local/motorController.py
+++ multithread/local/motorController.py
@@ -19,19 +19,47 @@ class MotorController(threading.Thread):
         self.x = 0
         self.y = 1
 
-    def findPort(self):
+    def findPort(self,port):
         global serialMotor
-        port = 0
         while port < 2:
             try:
                 serialMotor = SerialObject.initSerialObject("/dev/ttyACM" + str(port), True)
                 port += 1
-                return True
+                return True,port
             except serial.SerialException:
                 print("SerialException: device " + str(port) + " could not be found or could not be configured.")
                 port += 1
                 continue
-        return False
+        return False,port
+
+    def tryReceiveData(self):
+        rcv_str = serialMotor.read(10)
+        if(len(rcv_str) == 0):
+            return False
+        else:
+            logging.info("Motor: I don't want you, joy!")
+        return True
+
+    def tryStablishConnection(self):
+        isOpen = False
+        port = 0
+        while True:
+            isOpen,port = self.findPort(port)
+            if isOpen:
+                try:
+                    serialMotor.flushInput()
+                    if self.tryReceiveData() == False:
+                        break
+                except serial.SerialException:
+                    logging.info("Motor: SerialException: port closed.")
+                except Exception:
+                    logging.info("Motor: Flush input buffer error. (?)")
+                if serialMotor.isOpen():
+                    serialMotor.close()
+                isOpen = False
+            else:
+                port = 0
+
 
     def run(self):
         global serialMotor
@@ -39,46 +67,9 @@ class MotorController(threading.Thread):
             while globs.wait:
                 #print("Inside while wait!")
                 continue
-            isOpen = False
-            port = 0
-            while True:
-                while port < 2:
-                    try:
-                        serialMotor = SerialObject.initSerialObject("/dev/ttyACM" + str(port), True)
-                        isOpen = True
-                        port += 1
-                        break
-                    except serial.SerialException:
-                        print("SerialException: device " + str(port) + " could not be found or could not be configured.")
-                        port += 1
-                        continue
-                if isOpen:
-                    try:
-                        serialMotor.flushInput()
-                        rcv_str = serialMotor.read(10)
-                        if(len(rcv_str) == 0):
-                            break
-                        else:
-                            logging.info("Motor: I don't want you, joy!")
-                    except serial.SerialException:
-                        logging.info("Motor: SerialException: port closed.")
-                    except Exception:
-                        logging.info("Motor: Flush input buffer error. (?)")
-                    if serialMotor.isOpen():
-                        serialMotor.close()
-                    isOpen = False
-                else:
-                    port = 0
-                    #Talvez tirar esse else
+            self.tryStablishConnection()
             if serialMotor.isOpen():
                 try:
-					#reset_input_buffer()				# Flush input buffer, discarding all its contents
-					# reset_output_buffer()				# Flush output buffer, aborting current output
-									   					# and discard all that is in buffer
-					# Write data
-					#s.write("AT+CSQ")
-					#print("write data: AT+CSQ")
-					#time.sleep(0.5)  				# Give the serial port sometime to receive the data
                     logging.info("Motor: start writing...")
                     while True:
                         try:
