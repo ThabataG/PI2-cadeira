a1d73c5a74191c19ebc13853acfc68dacd04681b
Cotrim <cotrim149@gmail.com>
2015-12-10 01:50:56 -0200

    Organize imports of main.py

---
+++ Rasp Component/__init__.py
@@ -0,0 +1,20 @@
+import sys, os, inspect
+
+# Import defensive programming tools
+import logging
+logging.basicConfig(filename="", level=logging.DEBUG)
+
+# Relative referenciation of modules
+def referencePaths(pathsToImport):
+	for path in pathsToImport:
+		absolutePath = os.path.abspath(path)
+		if absolutePath not in sys.path:
+			sys.path.insert(0, absolutePath)
+
+# Referencing and importing modules
+# import sys, os, inspect
+pathsToImport = ["joystick", "motor", "communication"]
+referencePaths(pathsToImport)
+
+from Joystick import *
+from Motor import *
--- Rasp Component/main.py
+++ Rasp Component/main.py
@@ -2,25 +2,7 @@
   Universidade de Brasília - Campus Gama
   Disciplina: Projeto Integrador II
 """
-
-# Relative referenciation of modules
-def referencePaths(pathsToImport):
-	for path in pathsToImport:
-		absolutePath = os.path.abspath(path)
-		if absolutePath not in sys.path:
-			sys.path.insert(0, absolutePath)
-
-# Referencing and importing modules
-import sys, os, inspect
-pathsToImport = ["joystick", "motor", "communication"]
-referencePaths(pathsToImport)
-from Joystick import *
-from Motor import *
-
-# Import defensive programming tools
-import logging
-logging.basicConfig(filename="", level=logging.DEBUG)
-
+from __init__ import *
 # Main
 if __name__ == "__main__":
 	try:
