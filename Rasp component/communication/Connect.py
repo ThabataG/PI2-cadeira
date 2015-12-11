7ebc35bbdcf22515c9b591049cacb6f10d31dabb
duerno <felipeduernoal@gmail.com>
2015-12-11 02:07:33 -0200

    Remove some not needed comments
    signed-off-by: Duerno <felipeduernoal@gmail.com>
    signed-off-by: Cotrim <cotrim149@gmail.com>

--- Rasp component/communication/Connect.py
+++ Rasp component/communication/Connect.py
@@ -144,33 +144,9 @@ class Connect(object):
 			print("write error: Exception")
 		return writeWithSuccess
 
-
-	#
+	# 
 	@staticmethod
 	def close(serialObject):
 		if(serialObject.isOpen()):
 		#	logger.logger.info("Serial port closed")
 			serialObject.close()
-
-'''
-	#
-	@staticmethod
-	def tryReceiveData(obj):
-		obj.serial.flushInput()
-		rcv_str = obj.serial.read(10)
-		if "Joystick" in str(obj.__class__):
-			if(len(rcv_str) == 10):
-				isOpen = True
-			else:
-				logging.info("Joystick :Failed to read from serial.")
-				if obj.serial.isOpen():
-					obj.serial.close()
-				isOpen = False
-		elif "Motor" in str(obj.__class__):
-			if(len(rcv_str) == 0):
-				isOpen = False
-			else:
-				logging.info("Motor: I don't want you, joy!")
-				isOpen = True
-		return isOpen
-'''
